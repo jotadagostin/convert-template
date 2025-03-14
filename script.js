fdb37c5b2456936e8197065cb6003ddbdda7c3c2
João Paulo Dagostin <dagostinjota@gmail.com>
2025-03-14 22:14:26 +0100

    js update
    project ready

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // daily coin quote:
-const USD = 4.86;
-const EUR = 5.32;
-const GBP = 6.08;
+const USD = 5.73;
+const EUR = 6.23;
+const GBP = 7.41;
 
 // Getting the elements of the form
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulating the input amount to receive just number:
 amount.addEventListener("input", () => {
@@ -26,6 +29,7 @@ form.addEventListener("submit", (event) => {
 
     case "EUR":
       convertCurrency(amount.value, EUR, "€");
+      break;
 
     case "GBP":
       convertCurrency(amount.value, GBP, "£");
@@ -38,5 +42,38 @@ form.addEventListener("submit", (event) => {
 // function to convert the coin:
 
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol);
+  try {
+    // updating the coin quote
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)} `;
+    // to calculate the total
+    let total = amount * price;
+
+    // check is the result is not a number:
+    if (isNaN(total)) {
+      return alert("Please, type the correct number to convert");
+    }
+
+    // to form the total value:
+    total = formatCurrencyBRL(total).replace("R$", "");
+
+    // to show the total result
+    result.textContent = `${total} Reais`;
+
+    // aply the class the show the result
+    footer.classList.add("show-result");
+  } catch (error) {
+    console.log(error);
+    // remove the class that shows the result
+    footer.classList.remove("show-result");
+    alert("It was not possible to convert, try again later!");
+  }
+}
+
+// format in brazilian reais coin:
+function formatCurrencyBRL(value) {
+  // Convert to number to use the toLocaleString to format in BRL standart
+  return Number(value).toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
